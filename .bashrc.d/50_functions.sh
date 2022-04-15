3e4f0ba7e876ed91245fed4096a3c6cd0996b9ac
Jérôme Pin <jeromepin@users.noreply.github.com>
2022-04-15 18:09:48 +0200

    Setup Broot

--- .bashrc.d/50_functions.sh
+++ .bashrc.d/50_functions.sh
@@ -14,6 +14,21 @@ simple_http_server () {
     python3 -m http.server 8000 --bind 127.0.0.1
 }
 
+# broot --print-shell-function bash
+function br {
+    local cmd cmd_file code
+    cmd_file=$(mktemp)
+    if broot --outcmd "$cmd_file" "$@"; then
+        cmd=$(<"$cmd_file")
+        rm -f "$cmd_file"
+        eval "$cmd"
+    else
+        code=$?
+        rm -f "$cmd_file"
+        return "$code"
+    fi
+}
+
 complete -cf sudo
 complete -cf man
 complete -F __start_kubectl k
