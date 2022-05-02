37d4c930a3cf2114aecffb50c567592e35f750f5
Jérôme Pin <jeromepin@users.noreply.github.com>
2022-05-02 16:19:01 +0200

    Add more templates to OrgMode

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,4 +1,3 @@
--- Load custom tree-sitter grammar for org filetype
 require('orgmode').setup_ts_grammar()
 
 -- Doc : https://github.com/nvim-orgmode/orgmode/blob/master/DOCS.md#global-settings
@@ -22,26 +21,52 @@ require('orgmode').setup_ts_grammar()
 -- g? : show help
 require('orgmode').setup({
     org_agenda_files = {"~/org/*.org"},
+
     org_agenda_templates = {
         t = {
-          description = 'Task',
-          template = '* TODO %?\n  SCHEDULED: %t',
+            description = 'Task',
+            template = [[
+* TODO %?
+  SCHEDULED: %t]]
         },
+        m = {
+            description = 'Meetings',
+            template = [[
+* %t | %^{TITLE}
+  Attendees: Jérôme Pin
+
+  Notes:
+*** %?]]
+        },
+        n = {
+            description = 'Notes',
+            template = [[
+* %t | %^{TITLE}
+
+  Notes:
+*** %?]]
+        }
     },
-    
-    org_default_notes_file = "~/org/lumapps.org",
-    
+
+    org_default_notes_file = "~/org/refile.org",
+
     org_indent_mode = "noindent",
-    
+
     org_todo_keywords = {'TODO(t)', 'WAITING(w)', '|', 'DONE(d)', 'DELEGATED(e)'},
     org_todo_keyword_faces = {
         TODO = ':foreground red',
         WAITING = ':foreground orange',
         DONE = ':foreground green',
-        DELEGATED = ':slant italic :underline on',
+        DELEGATED = ':slant italic :underline on'
     },
+
+    mappings = {
+        org = {
+            org_todo = '<leader>ct'
+        }
+    }
 })
 
 require("org-bullets").setup({
-    symbols = { "◉", "○", "✸", "✿" }
+    symbols = {"◉", "○", "✸", "✿"}
 })
