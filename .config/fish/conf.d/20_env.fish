84bd565dac0456f839fb121d172d9dd007342487
Jérôme Pin <jeromepin@users.noreply.github.com>
2025-01-01 15:17:42 +0100

    fix paging

--- .config/fish/conf.d/20_env.fish
+++ .config/fish/conf.d/20_env.fish
@@ -1,9 +1,9 @@
-set -gx TERM screen-256color
+set -gx TERM xterm
 set -gx EDITOR "nvim"
 
-set -gx BAT_PAGER "less"
-set -gx PAGER "less"
-set -gx MANPAGER "sh -c 'col -bx | bat --style=plain --color=always --language=man --pager=less --paging=always'"
+set -gx BAT_PAGER "most"
+set -gx PAGER "less -eiRMX"
+set -gx MANPAGER "most"
 set -gx MANROFFOPT "-c"
 
 set -gx FZF_DEFAULT_COMMAND 'fd --follow --exclude .git'
