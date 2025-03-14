5cf5d5461a6c190fabb92736fb52c49977cbd5e1
Jérôme Pin <jeromepin@users.noreply.github.com>
2025-03-14 15:52:19 +0100

    Make nix-darwin use the same release version as nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -10,7 +10,7 @@
     nixpkgs-oldstable.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
